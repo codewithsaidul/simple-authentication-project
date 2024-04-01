b4311f57149a253b8eed685a5800b8bba037fecf
codewithsaidul <codewithsaidul@gmail.com>
2024-04-02 03:11:47 +0600

    Firebase Setup for Authentication

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../authProvider/AuthProvider";
 
 const Login = () => {
+
+  const authInfo = useContext(AuthContext);
+
+  
 
       const handleLogin= (e) => {
         e.preventDefault();
